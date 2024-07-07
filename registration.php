4da153f880cdebc4cfacaa3e567d4f85c308519c
Clifford Musyoka <your_email@example.com>
2024-07-07 21:14:35 +0300

    show errors

--- registration.php
+++ registration.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,24 +8,57 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-   <div class="container">
-   <div class="form-group">
-    <input type="text" class="form-control" name="fullname" placeholder="Full Name">
-   </div>
-   <div class="form-group">
-    <input type="email" class="form-control" name="email" placeholder="Email: ">
-   </div>
-   <div class="form-group">
-    <input type="password" class="form-control" name="password" placeholder="Password: ">
-   </div>
-   <div class="form-group">
-    <input type="password" class="form-control" name="repeat_password" placeholder="Repeat Password: ">
-   </div>
-   <div class="form-btn">
-    <input type="submit" class="btn btn-primary" name="submit" value="Register">
-   </div>
-   </div>
+    <div class="container">
+        <?php 
+        if(isset($_POST['submit'])){
+            $fullname = $_POST['fullname'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $passwordRepeat = $_POST['repeat_password'];
+            $errors = array();
+            if(empty($fullname) OR empty($email) OR empty($password) OR empty($passwordRepeat)){
+                array_push($errors, "All fields are required");
+            }
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                array_push($errors, "Email is not valid");
+            }
+            if(strlen($password)<8){
+              array_push($errors, "Password must be atleast 8  characters long"); 
+            }
+            if($password !== $passwordRepeat){
+                array_push($errors, "Password does not match");
+            }
+
+            if (count($errors)>0){
+                foreach ($errors as $error) { 
+                  echo "<div class='alert alert-danger'>$error</div>";
+                }
+            }else{
+                // insert data in the database
+            }
+        }
+        ?>
+        <form action="registration.php" method="post">
+        <div class="form-group">
+            <input type="text" class="form-control" name="fullname" placeholder="Full Name">
+        </div>
+        <div class="form-group">
+            <input type="email" class="form-control" name="email" placeholder="Email: ">
+        </div>
+        <div class="form-group">
+            <input type="password" class="form-control" name="password" placeholder="Password: ">
+        </div>
+        <div class="form-group">
+            <input type="password" class="form-control" name="repeat_password" placeholder="Repeat Password: ">
+        </div>
+        <div class="form-btn">
+            <input type="submit" class="btn btn-primary" name="submit" value="Register">
+        </div>
+        </form>
+    </div>
 
 </body>
+
 </html>
